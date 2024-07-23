60eab0ad498ee3afc3413d588e3eb2e42dbd6689
mdmoren <markmoreno34@gmail.com>
2024-07-22 22:30:21 -0400

    test ahuisdhfuishdgf

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,19 @@
-import azure from 'svelte-adapter-azure-swa';
+
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
-    kit: {
-        adapter: azure()
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      relative: false
     }
+  },
 };
